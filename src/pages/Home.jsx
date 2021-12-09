74aa5314618f4a8d034853496a427fe1712e91a1
eebbni <engeliss@naver.com>
2021-12-09 23:15:42 +0900

    app js수정

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React , {useState} from 'react';
+import styled from 'styled-components';
+
+const Homepage = styled.div`
+    min-width : 480px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+`;
+
+const Home = () => {
+   // const [todoList, setTodoList] = useState([]);
+
+    return (
+        <Homepage>
+           
+        </Homepage>
+    );
+}
+
+export default Home;
