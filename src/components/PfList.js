27af1d8722345413c6f30011d4401821f3ebe3bf
eebbni <engeliss@naver.com>
2021-12-20 00:51:18 +0900

    포트폴리오 개별 컴포넌트 추가

--- src/components/PfList.js
+++ src/components/PfList.js
@@ -23,15 +23,19 @@ const PfList = ({pfList,setPfList}) => {
     return(
         <ListBox>
             <List>
-               {pfList && //todoList가 있을때만 함
+               {pfList && //pfList가 있을때만 함
                 pfList.map((item)=>
                 { 
-                    //삭제된 투두는 출력하지 않음
+                    //삭제된 포트폴리오는 출력하지 않음
                     if(item.deleted) return null;
-                    console.log(item.title);
+
                     return (
-                       <>
-                       </>
+                        <PfItem 
+                            key={item.id}
+                            pfItem={item}
+                            pfList={pfList}
+                            setPfList={setPfList}
+                        />
                     )
                 }
                 )}
