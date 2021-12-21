892efa59f06f3646d3ede16b352f825d78fa1a8f
chaeyeon <engeliss@naver.com>
2021-12-21 17:49:06 +0900

    count 수정 ( 진행중인 프로젝트 표시)\

--- src/components/Count.js
+++ src/components/Count.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 
 export const Count = ({pfList}) => {
    return (
-    <div>전체 : {pfList.filter( item => !item.deleted).length }</div>
+    <div>전체 : {pfList.filter( item => !item.deleted).length } / 진행중인 프로젝트 : {pfList.filter( item => !item.deleted && !item.done).length }</div>
    )
 }
