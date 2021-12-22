d26a7a540d416e398b86d1060edcab90b045b92a
chaeyeon <engeliss@naver.com>
2021-12-22 10:05:25 +0900

    전체삭제 기능 추가

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,10 @@
-import React, {useState,useRef,useEffect} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import '../Portfolio.css';
 import {InputBox} from '../components/InputBox';
 import PfList from '../components/PfList';
 import {Count} from '../components/Count';
+import Button from '@mui/material/Button';
 
 const PortfolioMain = styled.main`
     display: block;
@@ -13,10 +14,27 @@ const PortfolioMain = styled.main`
 `;
 
 export const Portfolio = ({pfList,setPfList}) => {
+
+    const onClickAllDelete = () => {
+        if(window.confirm('정말 모든 포트폴리오를 지우겠습니까?'))
+        {
+            const allpfList = pfList.map((item)=>(
+                {
+                    ...item,
+                    deleted : true 
+                }
+            ));
+            setPfList(allpfList);
+        }
+    }
+
     return (
         <PortfolioMain>
             <InputBox pfList={pfList} setPfList={setPfList}/>
             <Count pfList={pfList}/>
+            <Button variant="outlined" color="error" onClick={onClickAllDelete}>
+                전체삭제
+            </Button>
             <PfList 
                 title={'진행중인 프로젝트'}
                 pfList={pfList}
